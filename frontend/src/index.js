24b8ac44baae25b21ee16538cb467d3c5c01aeb8
Kyle Sullivan <141261s@acadiau.ca>
2021-10-31 14:38:46 -0300

    Add chakraui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,15 @@ import { render } from 'react-dom'
 import { App } from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { Auth0ProviderWithHistory } from './Auth0ProviderWithHistory'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from './theme'
 
 render(
   <BrowserRouter>
     <Auth0ProviderWithHistory>
-      <App />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
     </Auth0ProviderWithHistory>
   </BrowserRouter>,
   document.getElementById('root'),
