ce0108f802091e49b71092849f1626a9bf34f778
Kyle Sullivan <141261s@acadiau.ca>
2021-11-25 20:00:52 -0400

    Resolve all subfolders

--- frontend/server.js
+++ frontend/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 const PORT = process.env.FRONTEND_PORT || 3000
 
-app.use('*', express.static('build'))
+app.use(express.static('build'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
